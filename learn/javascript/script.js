da66e700c08522ef44347a231af26de1c3030c01
waliou <brian891221@gmail.com>
2019-10-08 22:41:43 +0800

    Add files via upload
    first prototype

---
+++ learn/javascript/script.js
@@ -0,0 +1,10 @@
+function submit() {
+    var name = $('input[name=name]');
+    var score = $('input[name=score]');
+    var trial = $('input[name=trial]');
+    if(name.val()!=''&&score.val()!=''&&trial.val()!=''){
+        
+    }
+    else
+        alert("You didn't enter anything")
+}
