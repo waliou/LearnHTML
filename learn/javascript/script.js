583b5e505aa060f6149586b0b65ace3315be7405
waliou <brian891221@gmail.com>
2019-10-08 23:30:17 +0800

    IMPROVE: append & clear input
    1. new row will be added bottom
    2. input will automatically clear

--- learn/javascript/script.js
+++ learn/javascript/script.js
@@ -3,14 +3,15 @@ function submit() {
     var _score = $('input[name=score]');
     var _trial = $('input[name=trial]');
     if(_name.val()!=''&&_score.val()!=''&&_trial.val()!=''){
-        $('table[name=scoreboard]').bootstrapTable('insertRow',{
-            index:0,
-            row: {
-                name: _name.val(),
-                score: _score.val(),
-                trial:_trial.val()
-            }
+        $('table[name=scoreboard]').bootstrapTable('append',
+        {
+            name:_name.val(),
+            score:_score.val(),
+            trial:_trial.val()
         });
+        _name.val('');
+        _score.val('');
+        _trial.val('');
     }
     else
         alert("You have leaved something blank !!");
